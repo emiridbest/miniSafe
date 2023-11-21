a88eff722cd6fe797b2f1198b6e26083203ddb8b
emiridbest <udogwuemiri@gmail.com>
2023-11-21 07:49:04 +0100

    contract

--- contract/minisafe.sol
+++ contract/minisafe.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MiniSafe is ERC20, Ownable {
+contract MiniSafe is ERC20{
     struct TokenBalance {
         uint256 celoBalance;
         uint256 cUsdBalance;
